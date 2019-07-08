02d33ab7f8bcc30a6fecdf3c276d04f951cdc40c
Sam-Brodersen <samtalebi84@gmail.com>
2019-07-08 11:42:25 +0200

    createModel()

--- js/board.js
+++ js/board.js
@@ -2,7 +2,24 @@
 class Board{
   constructor(size){
     this.size = size;
-    this.board = this.createModel();
+    this.board = this._createModel();
+    this.elem = this._createView();
   }
+  // ------------------------------------------------------------------------
+  // Model and View methods
+  // ------------------------------------------------------------------------
 
+  _createModel(){
+    let model = [];
+
+    for(let row = 0; row < this.size; row++){
+      model.push([]) // will push a [] for each row
+
+      for(let column = 0; column < this.size; column++){
+        let id = `${r},${c}`;
+        model[row].push(new Square(id)); // push a new square for each column to the current row
+      }
+    }
+    return model;
+  }
 }
